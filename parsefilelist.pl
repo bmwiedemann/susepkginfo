912cab82bb9052446b0d5e7410e7b771128b0d81
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-05-31 20:35:34 +0200

    Parse apparmor file list again

--- parsefilelist.pl
+++ parsefilelist.pl
@@ -14,14 +14,19 @@ if(!$target || @ARGV) {usage}
 
 my %map;
 my $pkgname;
+open(APPARMORFD, ">", "db/apparmor-list.txt");
 
 while(<>) {
     if(m{<package .*name="([^"]+)".*>}) {
         $pkgname = $1;
     }
     if (m{^  <(file)>([^<]+)</\1>}) {
+        my $file = $2;
         #print "matched $1 $2\n";
-        $map{$2}=$pkgname;
+        $map{$file} = $pkgname;
+        if($file =~ m{^/etc/apparmor|^/usr/share/apparmor|^/etc/apparmor.d}) {
+            print APPARMORFD "$file=$pkgname\n";
+        }
     }
 }
 
