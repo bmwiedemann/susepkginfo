11bc062b7e06ec73315338c686990ceb424130d1
Bernhard M. Wiedemann <bwiedemann@suse.de>
2022-06-11 21:00:44 +0200

    Drop unused dependency

--- buildscript.sh
+++ buildscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
 zypper -n up
-zypper -n in --no-recommends apache2-mod_perl curl perl-CGI perl-core-DB_File perl-Crypt-SSLeay perl-JSON-XS perl-URI perl-XML-Simple wget make
+zypper -n in --no-recommends apache2-mod_perl curl perl-CGI perl-core-DB_File perl-JSON-XS perl-URI perl-XML-Simple wget make
 a2enmod perl
 chmod +x /usr/bin/entrypoint
