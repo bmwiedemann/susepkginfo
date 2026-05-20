6293cac84736dc2aceef1c284a02cfeab7a82545
Bernhard M. Wiedemann <bwiedemann@suse.de>
2026-05-20 07:42:10 +0200

    Fix install of DB_File

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 #zypper -n in sudo
 zypper -n up
-zypper -n in --no-recommends apache2-mod_perl perl-CGI perl-core-DB_File perl-JSON-XS perl-URI perl-XML-Simple wget make
+zypper -n in --no-recommends apache2-mod_perl perl-CGI 'perl(DB_File)' perl-JSON-XS perl-URI perl-XML-Simple wget make
 
 #wget -O /srv/www/cgi-bin/opensusemaintainer https://raw.githubusercontent.com/bmwiedemann/susepkginfo/master/opensusemaintainer
 chmod a+x /srv/www/cgi-bin/opensusemaintainer
